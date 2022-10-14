0a1e2cf7d1d1d7d2b687387a0df8d73b8e0a0a42
SainiShivanshu <shivanshusaini216@gmail.com>
2022-10-14 08:02:08 +0530

    9

---
+++ app/src/main/java/com/example/shoppieadmin/activity/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.example.shoppieadmin.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.shoppieadmin.MainActivity
+import com.example.shoppieadmin.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    var imageView: ImageView? = null
+    var charSequence: CharSequence? = null
+    var index = 0
+    var delay: Long = 200
+    var handler = Handler()
+    var textView: TextView? = null
+    var binding: ActivitySplashBinding?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+            finish()
+        }, 500)
+
+    }
+}
